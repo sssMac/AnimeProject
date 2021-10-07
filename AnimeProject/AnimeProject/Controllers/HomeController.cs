6b6aff8dd47917b1aeef0bc175166ed53f4fb85a
Ilya <ilya-lyapin@mail.ru>
2021-10-07 19:43:11 +0300

    Add login and reg page

---
+++ AnimeProject/AnimeProject/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
