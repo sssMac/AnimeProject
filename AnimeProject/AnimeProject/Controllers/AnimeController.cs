3b408856d8891d98f5ffe742bb5d1b8bf9c630d8
Ilya <ilya-lyapin@mail.ru>
2021-11-29 06:03:49 +0300

    Fix project

---
+++ AnimeProject/AnimeProject/Controllers/AnimeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeProject.Controllers
+{
+    public class AnimeController : Controller
+    {
+        // GET: AnimeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(string a)
+        {
+            return Ok(a);
+        }
+    }
+}
